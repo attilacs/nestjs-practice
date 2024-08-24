6ffb85fdd391aa2f58bfa3870273d6daae45503a
attilacs <csanyiattila23@gmail.com>
2024-08-24 14:33:37 +0200

    Update environment handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: `env/${process.env.NODE_ENV}.env`,
     }),
   ],
   controllers: [],
