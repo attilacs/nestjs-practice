146c3931d2de2fac14dbb024c69555f9651efbae
attilacs <csanyiattila23@gmail.com>
2024-08-25 13:51:03 +0200

    Implement roles guard

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    if (
+      !requiredRoles.some((role) => role === (user.isAdmin ? 'admin' : 'user'))
+    ) {
+      throw new ForbiddenException();
+    }
+
+    return true;
+  }
+}
